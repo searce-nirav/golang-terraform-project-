62ae3eda2116db8c457787db7f8d35286c005595
sahu04 <105695204+sahu04@users.noreply.github.com>
2022-12-20 13:32:20 +0530

    Update provider.tf

--- Devops/provider.tf
+++ Devops/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.aws_region
-  access_key = ""
-  secret_key = ""
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key =${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
